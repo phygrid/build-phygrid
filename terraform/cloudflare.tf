ea30058680da4b83a922fa898d0f01f6672dfc5f
kz <zoltan.kezsmarki@ombori.com>
2024-11-20 20:02:31 +0000

    TF fixes

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -1,48 +1,48 @@
-data "cloudflare_accounts" "console_deployment_cloudflare_account" {
+data "cloudflare_accounts" "cloudflare_account" {
   name = (lookup(local.cloudflare_settings, terraform.workspace)).account_name
 }
 
-data "cloudflare_zone" "console_deployment_cloudflare_zone" {
-  account_id = data.cloudflare_accounts.console_deployment_cloudflare_account.accounts[0].id
+data "cloudflare_zone" "cloudflare_zone" {
+  account_id = data.cloudflare_accounts.cloudflare_account.accounts[0].id
   name       = (lookup(local.cloudflare_settings, terraform.workspace)).zone_name
 }
 
-resource "cloudflare_pages_project" "console_deployment_cloudflare_pages_project" {
-  account_id        = data.cloudflare_accounts.console_deployment_cloudflare_account.accounts[0].id
+resource "cloudflare_pages_project" "cloudflare_pages_project" {
+  account_id        = data.cloudflare_accounts.cloudflare_account.accounts[0].id
   name              = lower("phygrid-build-${terraform.workspace}")
   production_branch = "main"
   depends_on = [
-    data.cloudflare_accounts.console_deployment_cloudflare_account
+    data.cloudflare_accounts.cloudflare_account
   ]
 }
 
-resource "cloudflare_pages_domain" "console_deployment_cloudflare_custom_domain" {
-  account_id   = data.cloudflare_accounts.console_deployment_cloudflare_account.accounts[0].id
-  project_name = cloudflare_pages_project.console_deployment_cloudflare_pages_project.name
+resource "cloudflare_pages_domain" "cloudflare_custom_domain" {
+  account_id   = data.cloudflare_accounts.cloudflare_account.accounts[0].id
+  project_name = cloudflare_pages_project.cloudflare_pages_project.name
   domain       = join(".", [(lookup(local.cloudflare_settings, terraform.workspace)).host, (lookup(local.cloudflare_settings, terraform.workspace)).zone_name])
   depends_on = [
-    data.cloudflare_accounts.console_deployment_cloudflare_account,
-    cloudflare_pages_project.console_deployment_cloudflare_pages_project
+    data.cloudflare_accounts.cloudflare_account,
+    cloudflare_pages_project.cloudflare_pages_project
   ]
 }
 
-resource "cloudflare_record" "console_deployment_cloudflare_cname_record" {
-  zone_id = data.cloudflare_zone.console_deployment_cloudflare_zone.id
+resource "cloudflare_record" "cloudflare_cname_record" {
+  zone_id = data.cloudflare_zone.cloudflare_zone.id
   name    = (lookup(local.cloudflare_settings, terraform.workspace)).host
-  value   = join(".", [cloudflare_pages_project.console_deployment_cloudflare_pages_project.name, "pages.dev"])
+  value   = join(".", [cloudflare_pages_project.cloudflare_pages_project.name, "pages.dev"])
   type    = "CNAME"
   ttl     = 1
   proxied = true
 }
 
-output "console_deployment_cloudflare_account_id" {
-  value = data.cloudflare_accounts.console_deployment_cloudflare_account.accounts[0].id
+output "cloudflare_account_id" {
+  value = data.cloudflare_accounts.cloudflare_account.accounts[0].id
 }
 
-output "console_deployment_cloudflare_pages_name" {
-  value = cloudflare_pages_project.console_deployment_cloudflare_pages_project.name
+output "cloudflare_pages_name" {
+  value = cloudflare_pages_project.cloudflare_pages_project.name
 }
 
-output "console_deployment_cloudflare_token" {
+output "cloudflare_token" {
   value = nonsensitive(data.azurerm_key_vault_secret.cloudflare_token.value)
 }
